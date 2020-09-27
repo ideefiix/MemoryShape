ae65dffe466358bfe0088ba1f810de4d17e3afd1
KesusKristus <57448724+KesusKristus@users.noreply.github.com>
2020-09-27 22:20:19 +0200

    fortsatt med scenebuilder

--- src/main/java/chalmers/app/controller/MenuController.java
+++ src/main/java/chalmers/app/controller/MenuController.java
@@ -1,4 +1,36 @@
 package chalmers.app.controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class MenuController {
+
+    @FXML
+    Panel menuStartButton;
+    @FXML
+    Panel menuCustomButton;
+    @FXML
+    Panel menuLeaderboardButton;
+    @FXML
+    Panel menuOptionsButton;
+    @FXML
+    Panel menuExitButton;
+
+    private void startButtonPressed(){
+    }
+
+    private void customButtonPressed(){
+    }
+
+    private void leaderboardButtonPressed(){
+    }
+
+    private void optionsButtonPressed(){
+    }
+
+    private void exitButtonPressed(){
+    }
+
+
 }
