82a813532722eb8ed0267456322993eb1c43403e
idefiix <flindu@gmail.com>
2020-10-14 09:59:11 +0200

    Bugfix

--- src/main/java/chalmers/app/controller/CardController.java
+++ src/main/java/chalmers/app/controller/CardController.java
@@ -3,23 +3,13 @@ package chalmers.app.controller;
 import chalmers.app.model.Card;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
 
 public class CardController extends AnchorPane  {
     private BoardController parentController;
@@ -71,9 +61,8 @@ public class CardController extends AnchorPane  {
             parentController.incScore();
             parentController.getGame().getCardSelector().changeSelectedCard();
             parentController.setSelectedCard();
-            // Will create a new board if the board is completed
 
-            // TODO FIX this method
+            // Will create a new board if the board is completed
             parentController.isLevelCompleted();
         }
         else{
