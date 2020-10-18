0c1c5d8d69ec2fbd42c620c02d6eb087b9a5b236
idefiix <flindu@gmail.com>
2020-10-18 15:37:17 +0200

    Created JSON file with highscores

--- src/main/java/chalmers/app/controller/MenuController.java
+++ src/main/java/chalmers/app/controller/MenuController.java
@@ -1,20 +1,34 @@
 package chalmers.app.controller;
 
+import chalmers.app.model.Highscore;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javax.swing.text.html.ImageView;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 
-public class MenuController {
+public class MenuController implements Initializable {
 
     private MainController mainController;
+    private List<Highscore> hList;
 
     @FXML
     AnchorPane menuAnchorPane;
@@ -26,6 +40,8 @@ public class MenuController {
     AnchorPane modeAnchorPane;
     @FXML
     AnchorPane soundOptionsPane;
+    @FXML
+    FlowPane highscore_flwPane;
 
 
 
@@ -33,6 +49,69 @@ public class MenuController {
 
     public MenuController(MainController mainController) {
         this.mainController = mainController;
+        loadHighscores();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fill_leaderboard();
+    }
+
+    /**
+     * Load 10 Highscores from JSON file
+     */
+
+    void loadHighscores(){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = new FileInputStream(new File("src/main/resources/highscores.json"));
+            hList = mapper.readValue(inputStream, new TypeReference<List<Highscore>>() {});
+            for(int i = 0; i < hList.size(); i++){
+                System.out.println(hList.get(i).getName());
+            }
+
+            inputStream.close();
+
+        } catch (FileNotFoundException e){
+
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Fills the flowpane with HighScoreController
+     */
+    private void fill_leaderboard() {
+        highscore_flwPane.getChildren().clear();
+
+        for(int i = 0; i < hList.size(); i++){
+            HighscoreController hc = new HighscoreController(this, String.valueOf(i+1), hList.get(i).getName(),hList.get(i).getMode(),hList.get(i).getScore());
+            highscore_flwPane.getChildren().add(hc);
+        }
+
+    }
+
+    /**
+     * TODO Called when program is shut down
+     * Writes the highScores to JSON file
+     */
+    void writeHighscores() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = new FileInputStream(new File("src/main/resources/highscores.json"));
+            mapper.writeValue(new File("src/main/resources/highscores.json"), hList);
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+                e.printStackTrace();
+            }
+
     }
 
     @FXML
@@ -76,7 +155,5 @@ public class MenuController {
     void newPlayerButtonPressed(){
         playerNameAnchorPane.toFront();
     }
-
-
 
 }
